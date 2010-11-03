0e4656cdc8a31e2cd1f8d59221ab938455031fd2
Trey Dempsey <trey.dempsey@gmail.com>
2010-11-03 00:32:30 -0500

    Initial check in

---
+++ src/dependencies.h
@@ -0,0 +1,34 @@
+/*
+ *  Copyright 2010  Trey Dempsey
+ *
+ *  libtgcl is distributed under the terms of the GNU
+ *  Lesser General Public License.
+ * 
+ *  This file is part of libtgcl.
+ *
+ *  libtgcl is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation, either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  libtgcl is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with libtgcl.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ ***********************************************************************/
+
+#ifndef _DEPENDENCIES_H_
+#define _DEPENDENCIES_H_
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#endif /* not _DEPENDENCIES_H_ */
